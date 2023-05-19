bfb0fe990ad8e3161b02b4f433eb4df1b216388b
marcosismayle <marcosismayle@gmail.com>
2023-05-19 19:57:45 -0300

    dom exercise updated

--- lesson5/scripture-study-list/scripts/script.js
+++ lesson5/scripture-study-list/scripts/script.js
@@ -3,7 +3,10 @@ const whatStudy = document.querySelector('input');
 const addSubject = document.querySelector('button');
 
 addSubject.addEventListener('click', () => {
-    const mySubject = whatStudy.value;
+    const mySubject = whatStudy.value.trim();
+    if (mySubject === '') {
+        return;
+    }
     whatStudy.value = '';
 
     const listSubject = document.createElement('li');
@@ -21,5 +24,21 @@ addSubject.addEventListener('click', () => {
     });
 
     whatStudy.focus();
-
 });
+
+const d = new Date();
+let currentYear = d.getFullYear();
+
+document.getElementById("currentYear").innerHTML = currentYear;
+
+let lastmodified = new Date(document.lastModified);
+
+let month = lastmodified.getMonth() + 1; // getMonth() returns a 0-based value, so adding 1 to match the correct month
+let date = lastmodified.getDate();
+let year = lastmodified.getFullYear();
+let hours = lastmodified.getHours();
+let minutes = lastmodified.getMinutes();
+let seconds = lastmodified.getSeconds();
+let dateString = `Last Updated: ${month}/${date}/${year} - ${hours}:${minutes}:${seconds}`;
+
+document.getElementById("lastUpdated").innerHTML = dateString;
