d13f5195c26839819f28f273c77571c3d12d591a
marcosismayle <marcosismayle@gmail.com>
2023-05-31 17:37:40 -0300

    Chamber site updated

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,21 +1,22 @@
-function doInputOutput() {
-
-    // get inputs from fields
-    let t = parseFloat(document.getElementById("tempInput").value);
-    let s = parseFloat(document.getElementById("speedInput").value);
-
-    //call windChill() function to the variable chill and apply the users inputs
-    let chill = windChill(t, s);
-
-    //output the result to a user
-    document.getElementById("output").innerHTML = chill.toFixed(1) + ' &deg;F';
-
-
-}
 function windChill(tempF, speed) {
 
     //temperature factor equation
     let f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
     //return variable f
     return f;
+}
+
+// get inputs from fields
+let t = parseFloat(document.getElementById("tempInput").innerHTML);
+let s = parseFloat(document.getElementById("speedInput").innerHTML);
+
+//call windChill() function to the variable chill and apply the users inputs
+let chill = windChill(t, s);
+
+//output the result to a user
+
+if (t <= 50 && s > 3.0) {
+    document.getElementById("output").innerHTML = chill.toFixed(1) + ' &deg;F';
+} else {
+    document.getElementById("output").innerHTML ='N/A';
 }
