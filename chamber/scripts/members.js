d5717b6811b93b53a3f2d9cf2bc72c09bfc0ddfd
marcosismayle <marcosismayle@gmail.com>
2023-06-23 16:54:23 -0300

    members updated

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -10,34 +10,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayCompanies = (companies) => {
     const grid = document.querySelector('div.grid');
 
-    if (grid) {
-      companies.forEach((company) => {
-        let card = document.createElement('section');
-        let logo = document.createElement('img');
-        let name = document.createElement('h2');
-        let address = document.createElement('p');
-        let phone = document.createElement('p');
-        let website = document.createElement('a');
+    companies.forEach((company) => {
+      let card = document.createElement('section');
+      let logo = document.createElement('img');
+      let name = document.createElement('h2');
+      let address = document.createElement('p');
+      let phone = document.createElement('p');
+      let website = document.createElement('a');
 
-        name.innerHTML = `${company.name}`;
-        address.innerHTML = `${company.address}`;
-        phone.innerHTML = `${company.phone}`;
-        website.innerHTML = `${company.website}`;
-        website.href = `${company.website}`;
+      name.innerHTML = `${company.name}`;
+      address.innerHTML = `${company.address}`;
+      phone.innerHTML = `${company.phone}`;
+      website.innerHTML = `${company.website}`;
+      website.href = `${company.website}`;
 
-        logo.setAttribute('src', company.imageurl);
-        logo.setAttribute('alt', `Logo of ${company.name}`);
-        logo.setAttribute('loading', 'lazy');
+      logo.setAttribute('src', company.imageurl);
+      logo.setAttribute('alt', `Logo of ${company.name}`);
+      logo.setAttribute('loading', 'lazy');
 
-        card.appendChild(logo);
-        card.appendChild(name);
-        card.appendChild(address);
-        card.appendChild(phone);
-        card.appendChild(website);
+      card.appendChild(logo);
+      card.appendChild(name);
+      card.appendChild(address);
+      card.appendChild(phone);
+      card.appendChild(website);
 
-        grid.appendChild(card);
-      });
-    }
+      grid.appendChild(card);
+    });
   }
 
   getCompanyData(localData)
@@ -49,21 +47,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const listbutton = document.querySelector("#list");
   const display = document.querySelector(".grid");
 
-  if (gridbutton && listbutton && display) {
-    gridbutton.addEventListener("click", () => {
-      display.classList.add("grid");
-      display.classList.remove("list");
-    });
+  gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+  });
 
-    listbutton.addEventListener("click", () => {
-      display.classList.remove("grid");
-      display.classList.add("list");
-    });
-  }
+  listbutton.addEventListener("click", () => {
+    display.classList.remove("grid");
+    display.classList.add("list");
+  });
 });
-
-
-
-
-
-
